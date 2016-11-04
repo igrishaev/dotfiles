c39627c355a19be98aa444bbdcbc9b68bd0ca589
Ivan Grishaev <ivan@grishaev.me>
2016-11-04 13:58:47 +0300

    soft file added

---
+++ software.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+brew cask install \
+     sketch \
+     skype \
+     google-chrome \
+     chromium \
+     slack \
+     pgadmin4 \
+     emacs \
+     docker \
+     firefox \
+     kdiff3 \
+     virtualbox \
+     torbrowser
