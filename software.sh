caad8c2b9a51816b76da2342c5e826c28a17bc8c
Ivan Grishaev <ivan@grishaev.me>
2016-11-07 22:52:29 +0300

    soft added

--- software.sh
+++ software.sh
@@ -1,26 +1,41 @@
 #!/bin/bash
 
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
 brew tap caskroom/cask
+brew tap homebrew/services
 
 brew install \
      git \
+     leiningen \
+     mercurial \
      wget \
+     cmake \
+     gcc \
+     mc \
      curl \
-     pandoc \
+     markdown \
+     zsh \
      ghc \
+     postgresql \
+     python3 \
+     pandoc \
      mactex
 
 brew cask install \
+     1password \
      iterm2 \
-     sketch \
      skype \
      google-chrome \
      chromium \
      slack \
+     franz \
      pgadmin4 \
      emacs \
+     java \
      docker \
      firefox \
      kdiff3 \
      virtualbox \
+     sketch \
      torbrowser
