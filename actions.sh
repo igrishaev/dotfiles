ec9da1e1b422a54315c1bab14e33436fa3919f36
Ivan Grishaev <ivan@grishaev.me>
2020-08-18 15:51:42 +0300

    updated

--- actions.sh
+++ actions.sh
@@ -6,7 +6,7 @@
 brew install git leiningen wget mc markdown zsh gettext gpg pinentry-mac ctags aspell clojure geckodriver youtube-dl
 
 # casks
-brew cask install opensc iterm2 google-chrome firefox slack emacs java docker kdiff3 telegram symboliclinker vlc obs karabiner-elements microsoft-excel microsoft-word sublime-text zoomus chromedriver monodraw openemu
+brew cask install opensc iterm2 google-chrome firefox slack emacs java docker kdiff3 telegram symboliclinker vlc obs karabiner-elements microsoft-excel microsoft-word sublime-text zoomus chromedriver monodraw openemu xquartz
 
 # oh-my-zhs
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
