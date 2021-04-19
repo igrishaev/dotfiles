608f33b1be57e905c4d104a6d3a9143bb9a179a7
Ivan Grishaev <ivan@grishaev.me>
2021-04-19 17:09:59 +0300

    updated

--- key-remap.sh
+++ key-remap.sh
@@ -5,6 +5,7 @@
 
 hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000039,"HIDKeyboardModifierMappingDst":0x7000000E1},{"HIDKeyboardModifierMappingSrc":0x7000000E7,"HIDKeyboardModifierMappingDst":0x7000000E6},{"HIDKeyboardModifierMappingSrc":0x7000000E6,"HIDKeyboardModifierMappingDst":0x7000000E4}]}'
 
+hidutil property --get "UserKeyMapping"
 
 # caps          left shift
 # 0x39          0xE1
